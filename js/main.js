fb415393d390d3731694c4ccc75e8ab03303c23c
Guido Quagliardi <guidoquagliardi@gmail.com>
2023-05-04 04:45:49 +0200

    Subida segunda pre-entrega JS

---
+++ js/main.js
@@ -0,0 +1,546 @@
+
+
+/**********************************
+***********************************
+    MI ARRAY
+        DE
+    PRODUCTOS
+************************************
+************************************/
+
+let productos = [
+
+    /******************** 
+    
+    === ZAPATILLAS ===
+
+    ********************/
+
+    {
+        id: "zapatilla-01",
+        titulo: "Nike Zoom Freak 3 'Crimson Bliss'",
+        imagen: "./img/nike/nike-01-1.png",
+        precio: "59320",
+        marca: "Nike",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-02",
+        titulo: "Nike Precision 6 'Iron Grey'",
+        imagen: "./img/nike/nike-02-1.png",
+        precio: "50420",
+        marca: "Nike",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-03",
+        titulo: "Nike Zoom Freak 4 'Greek Coastline'",
+        imagen: "./img/nike/nike-03-1.png",
+        precio: "62405",
+        marca: "Nike",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-04",
+        titulo: "Nike Kyrie 6 - TB Black",
+        imagen: "./img/nike/nike-04-1.png",
+        precio: "57900",
+        marca: "Nike",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-05",
+        titulo: "Luka 1 'Next Nature'",
+        imagen: "./img/jordan/jordan-01-1.webp",
+        precio: "67200",
+        marca: "Jordan",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-06",
+        titulo: "Air Jordan XXXVII Satou",
+        imagen: "./img/jordan/jordan-02-1.jpg",
+        precio: "55500",
+        marca: "Jordan",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-07",
+        titulo: "Air Jordan XXXVII Low",
+        imagen: "./img/jordan/jordan-03-1.webp",
+        precio: "46150",
+        marca: "Jordan",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-08",
+        titulo: "Air Jordan XXXVI Low",
+        imagen: "./img/jordan/jordan-04-1.webp",
+        precio: "63170",
+        marca: "Jordan",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-09",
+        titulo: "Under Armour Buzzer 'Electrical'",
+        imagen: "./img/under_armour/under_armour-01-1.jpg",
+        precio: "58250",
+        marca: "Under Armour",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-10",
+        titulo: "Under Armour Spawn 3 'Black Beta'",
+        imagen: "./img/under_armour/under_armour-02-1.png",
+        precio: "54860",
+        marca: "Under Armour",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-11",
+        titulo: "Under Armour Curry 3Zer0 3 'Grey Gravity Green'",
+        imagen: "./img/under_armour/under_armour-03-1.webp",
+        precio: "41358",
+        marca: "Under Armour",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-12",
+        titulo: "Under Armour Spawn 3 'Electric Blue'",
+        imagen: "./img/under_armour/under_armour-04-1.png",
+        precio: "54860",
+        marca: "Under Armour",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-13",
+        titulo: "Adidas DON Issue 3 'Christmas'",
+        imagen: "./img/adidas/adidas-01-1.png",
+        precio: "62180",
+        marca: "Adidas",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-14",
+        titulo: "Adidas Harden Stepback 2 'Aqua'",
+        imagen: "./img/adidas/adidas-02-1.png",
+        precio: "48170",
+        marca: "Adidas",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-15",
+        titulo: "Adidas DON Issue 2 Crayola 'Solar Gold'",
+        imagen: "./img/adidas/adidas-03-1.png",
+        precio: "60000",
+        marca: "Adidas",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    {
+        id: "zapatilla-16",
+        titulo: "Adidas Harden Vol.5 Futurenatural 'CNY'",
+        imagen: "./img/adidas/adidas-04-1.png",
+        precio: "54550",
+        marca: "Adidas",
+        categoria: {
+            nombre: "calzado",
+            producto: "zapatillas",
+        },
+    },
+
+    /******************** 
+    
+    === INDUMENTARIA ===
+
+    ********************/
+
+    // === CAMISETAS ===
+    {
+        id: "indumentaria-01",
+        titulo: "Camiseta 'Classic'",
+        imagen: "./img/indumentaria-jordan-01-1.jpg",
+        precio: "7800",
+        marca: "Jordan",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "camisetas, remeras",
+        },
+    },
+
+    {
+        id: "indumentaria-02",
+        titulo: "Camiseta Nike 'Golden'",
+        imagen: "./img/indumentaria-nike-02-1.jpg",
+        precio: "8800",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "camisetas, remeras",
+        },
+    },
+
+    {
+        id: "indumentaria-03",
+        titulo: "Camiseta Nike 'Lakers'",
+        imagen: "./img/indumentaria-nike-03-1.webp",
+        precio: "9250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "camisetas, remeras",
+        },
+    },
+
+    {
+        id: "indumentaria-04",
+        titulo: "Camiseta Nike 'Brooklyn'",
+        imagen: "./img/indumentaria-nike-04-1.webp",
+        precio: "9250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "camisetas, remeras",
+        },
+    },
+
+    // === BUZOS ===
+    {
+        id: "indumentaria-05",
+        titulo: "Buzo Canguro 'Lakers'",
+        imagen: "./img/indumentaria-nike-05-1.jpg",
+        precio: "24320",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "buzos canguros",
+        },
+    },
+
+    {
+        id: "indumentaria-06",
+        titulo: "Buzo Canguro 'NBA'",
+        imagen: "./img/indumentaria-nike-06-1.jpg",
+        precio: "22420",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "buzos canguros",
+        },
+    },
+
+    {
+        id: "indumentaria-07",
+        titulo: "Buzo Canguro 'Bulls'",
+        imagen: "./img/indumentaria-nike-07-1.webp",
+        precio: "24320",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "buzos canguros",
+        },
+    },
+
+    {
+        id: "indumentaria-08",
+        titulo: "Buzo Canguro 'Suns'",
+        imagen: "./img/indumentaria-nike-08-1.jpg",
+        precio: "24320",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "buzos canguros",
+        },
+    },
+
+    //=== PANTALONES ===
+
+    {
+        id: "indumentaria-09",
+        titulo: "Pantalón Largo 'Bulls'",
+        imagen: "./img/indumentaria-nike-09-1.webp",
+        precio: "19250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "pantalones largos",
+        },
+    },
+
+    {
+        id: "indumentaria-10",
+        titulo: "Pantalón Largo 'Lakers'",
+        imagen: "./img/indumentaria-nike-10-1.webp",
+        precio: "19250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "pantalones largos",
+        },
+    },
+
+    {
+        id: "indumentaria-11",
+        titulo: "Pantalón Largo 'Wizards'",
+        imagen: "./img/indumentaria-nike-11-1.webp",
+        precio: "19250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "pantalones largos",
+        },
+    },
+
+    {
+        id: "indumentaria-12",
+        titulo: "Pantalón Largo 'Brooklyn'",
+        imagen: "./img/indumentaria-nike-12-1.webp",
+        precio: "19250",
+        marca: "Nike",
+        categoria: {
+            nombre: "indumentaria",
+            producto: "pantalones largos",
+        },
+    },
+
+        /******************** 
+    
+    === ACCESORIOS ===
+
+    ********************/
+
+    {
+        id: "accesorios-01",
+        titulo: "Mochila Jordan 'Classic'",
+        imagen: "./img/accesorios-mochila-01-1.jpg",
+        precio: "29670",
+        marca: "Jordan",
+        categoria: {
+            nombre: "acessorios",
+            producto: "mochilas",
+        },
+    },
+
+    {
+        id: "accesorios-02",
+        titulo: "Mochila Jordan 'Power USA'",
+        imagen: "./img/accesorios-mochila-02-1.webp",
+        precio: "48280",
+        marca: "Jordan",
+        categoria: {
+            nombre: "acessorios",
+            producto: "mochilas",
+        },
+    },
+
+    {
+        id: "accesorios-03",
+        titulo: "Medias Jordan - Pack Three",
+        imagen: "./img/accesorios-medias-03-1.webp",
+        precio: "6405",
+        marca: "Jordan",
+        categoria: {
+            nombre: "acessorios",
+            producto: "medias",
+        },
+    },
+
+    {
+        id: "accesorios-04",
+        titulo: "Gorra Jordan 'Red Power'",
+        imagen: "./img/accesorios-gorra-04-1.jpg",
+        precio: "8255",
+        marca: "Jordan",
+        categoria: {
+            nombre: "acessorios",
+            producto: "gorras",
+        },
+    },
+
+    {
+        id: "accesorios-05",
+        titulo: "Pelota Molten - BG4500 N7",
+        imagen: "./img/accesorios-pelota-05-1.jpg",
+        precio: "45720",
+        marca: "Molten",
+        categoria: {
+            nombre: "acessorios",
+            producto: "pelotas",
+        },
+    },
+
+    {
+        id: "accesorios-06",
+        titulo: "Pelota Molten - GR5X 'Blue'",
+        imagen: "./img/accesorios-pelota-06-1.jpg",
+        precio: "14910",
+        marca: "Molten",
+        categoria: {
+            nombre: "acessorios",
+            producto: "pelotas",
+        },
+    },
+
+    {
+        id: "accesorios-07",
+        titulo: "Pelota Adidas N7 'Classic'",
+        imagen: "./img/accesorios-pelota-07-1.jpg",
+        precio: "38346",
+        marca: "Adidas",
+        categoria: {
+            nombre: "acessorios",
+            producto: "pelotas",
+        },
+    },
+
+    {
+        id: "accesorios-08",
+        titulo: "Carpeta Spalding 'NBA'",
+        imagen: "./img/accesorios-carpeta-08-1.jpg",
+        precio: "7200",
+        marca: "Spalding",
+        categoria: {
+            nombre: "acessorios",
+            producto: "carpetas",
+        },
+    },
+
+]
+
+//Declaración del título general
+
+const titulos = document.querySelector(".contenedor-titulos");
+titulos.innerText = "Todos los Productos";
+
+
+/**************************************
+***************************************
+
+FUNCION CARGAR PRODUCTOS
+
+***************************************
+***************************************/
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+
+
+function cargarProductos(productos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.innerHTML= `
+                    <div class="contenedor-card">
+                        <div class="contenedor-card-imagen">
+                            <img class="imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                        </div>
+                        <div class="contenedor-card-informacion">
+                            <h3 class="titulo mb-2">${producto.titulo}</h3>
+                            <div class="precio mb-2">$${producto.precio}</div>
+                            <button type="button" class="btn btn-dark" id="${producto.id}"><i class="bi bi-bag-fill"></i> AGREGAR</button>
+                        </div>
+                    </div>
+                
+        `;
+
+        contenedorProductos.append(div);
+    })
+}
+
+cargarProductos(productos);
+
+
+/**************************************
+***************************************
+
+FUNCION BUSCAR PRODUCTOS
+
+***************************************
+***************************************/
+
+const botonBuscar = document.querySelector(".btn__mio");
+
+
+botonBuscar.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const buscar = document.querySelector("#buscarProducto");
+    const productoBuscado = buscar.value.toLowerCase();
+
+    const resultados = productos.filter(function(producto) {
+    return (producto.titulo.toLowerCase().includes(productoBuscado) || producto.categoria.nombre.toLowerCase().includes(productoBuscado) || producto.categoria.producto.toLowerCase().includes(productoBuscado));
+    });
+
+    titulos.innerText = "Resultados de la busquedad"; //Cambio de título
+
+    if (resultados.length > 0) {
+        cargarProductos(resultados); //Reutilizo la función cargarProductos cambiando el parámetro
+    } else {
+        contenedorProductos.innerHTML = 
+                                    `<div class="contenedor__busquedad">
+                                        <p class="text-center pt-3">No se encontró "<b>${productoBuscado}</b>"</p>
+                                    </div>`;
+    }
+
+});
